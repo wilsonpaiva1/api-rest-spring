1631118011a749baae42fa92c0430c84beeb9e4f
wilsonpaiva1 <112273995+wilsonpaiva1@users.noreply.github.com>
2022-10-10 22:30:00 -0300

    Update TestConfigs.java
    porta 80

--- src/test/java/br/com/wilson/paiva/apirestspring/configs/TestConfigs.java
+++ src/test/java/br/com/wilson/paiva/apirestspring/configs/TestConfigs.java
@@ -1,7 +1,7 @@
 package br.com.wilson.paiva.apirestspring.configs;
 
 public class TestConfigs {
-    public static final int SERVER_PORT = 8080;
+    public static final int SERVER_PORT = 80;
     public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
     public static final String HEADER_PARAM_ORIGIN = "Origin";
 
